7123534cfcc58721ac9f4ce2a704377f77e28752
Mohammad Hossein Jafari <mhj10mhj10jafari@gmail.com>
2023-08-06 16:32:20 +0330

    edit quizContext

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,40 @@
 import { StateContext } from "./hooks/stateContext";
-import { Navigate, useRoutes } from 'react-router-dom';
-import Form from "./pages/form"
+import { Navigate, useRoutes } from "react-router-dom";
+import Form from "./pages/form";
 import NotFound from "./pages/not-found";
-import Quiz from "./pages/quiz"
+import Quiz from "./pages/quiz";
 import Result from "./pages/result";
+import quizProvider from "./contexts/quizContext";
 
 const App = () => {
+  const routes = useRoutes([
+    {
+      path: "/",
+      element: <Form />,
+    },
+    {
+      path: "/quiz",
+      element: <Quiz />,
+    },
+    {
+      path: "/result",
+      element: <Result />,
+    },
+    {
+      path: "/not-found",
+      element: <NotFound />,
+    },
+    {
+      path: "*",
+      element: <Navigate to="/not-found" replace />,
+    },
+  ]);
 
-   const routes = useRoutes([
-      {
-         path: "/", element: <Form/> 
-      },
-      {
-         path: "/quiz", element: <Quiz/> 
-      },
-      {
-         path: "/result", element: <Result/> 
-      },
-      {
-         path: "/not-found", element: <NotFound/>
-      },
-      {
-         path: "*" , element: <Navigate to="/not-found" replace />
-      }
-   ])
-
-   return (
-    <StateContext>
-      {routes}
-   </StateContext>
-  )
-}
-  
+  return (
+    <quizProvider>
+      <StateContext>{routes} </StateContext>
+    </quizProvider>
+  );
+};
 
 export default App;
--- src/contexts/quizContext.tsx
+++ src/contexts/quizContext.tsx
@@ -1,26 +1,11 @@
-import { createContext, Dispatch, ReactNode, useReducer } from "react";
-import quizReducer, { QuizAction, QuizInfo } from "../reducers/quizReducer";
+import { createContext, Dispatch } from "react";
+import { QuizAction, QuizInfo } from "../reducers/quizReducer";
 
 interface QuizContextType {
   quizInfo: QuizInfo;
   dispatch: Dispatch<QuizAction>;
 }
-interface Children {
-  children: ReactNode;
-}
 
-export const quizContext = createContext<QuizContextType>(
-  {} as QuizContextType
-);
+const quizContext = createContext<QuizContextType>({} as QuizContextType);
 
-const quizProvider = ({ children }: Children) => {
-  const [quizInfo, dispatch] = useReducer(quizReducer, {});
-
-  return (
-    <quizContext.Provider value={{ quizInfo, dispatch }}>
-      {children}
-    </quizContext.Provider>
-  );
-};
-
-export default quizProvider;
+export default quizContext;
