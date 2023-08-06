5cf7aa903bebcbc36aee74e726fc169b79eff5e5
Mohammad Hossein Jafari <mhj10mhj10jafari@gmail.com>
2023-08-06 16:17:29 +0330

    create quizReducer

---
+++ src/reducers/quizReducer.ts
@@ -0,0 +1,25 @@
+interface QuizInfo {
+  name: string;
+  amount: number;
+  category: number;
+}
+
+export interface QuizAction {
+  quizInfo: QuizInfo;
+  type: "SET_NAME" | "SET_AMOUNT" | "SET_CATEGORY";
+}
+
+const quizReducer = (state: QuizInfo, action: QuizAction) => {
+  switch (action.type) {
+    case "SET_NAME":
+      return { ...state, name: action.quizInfo.name };
+    case "SET_AMOUNT":
+      return { ...state, amount: action.quizInfo.amount };
+    case "SET_CATEGORY":
+      return { ...state, category: action.quizInfo.category };
+    default:
+      return state;
+  }
+};
+
+export default quizReducer;
