a5690005261d9396875cb255fe6950583cf09b4b
Mohammad Hossein Jafari <mhj10mhj10jafari@gmail.com>
2023-08-06 16:44:58 +0330

    add quizInfo and dispatch to quiz

--- src/pages/quiz.tsx
+++ src/pages/quiz.tsx
@@ -1,62 +1,67 @@
-import React, {useState, useContext} from "react";
+import React, { useState, useContext } from "react";
 import { AppContext } from "../hooks/stateContext";
 import { LoadQuestionHook } from "../hooks/loadQuestionsHook";
 import QuestionCard from "../components/questionCard";
+import quizContext from "../contexts/quizContext";
 
 const Quiz = () => {
-  const{questions, setQuestions} = useContext(AppContext)
-  const[number, setNumber] = useState<number>(0)
-  const{userAnswers, setUserAnswers, quizInfo } = useContext(AppContext) 
+  const { questions, setQuestions } = useContext(AppContext);
+  const [number, setNumber] = useState<number>(0);
+  const { userAnswers, setUserAnswers } = useContext(AppContext);
+  const { quizInfo } = useContext(quizContext);
 
-  LoadQuestionHook(quizInfo.amount, quizInfo.category, setQuestions)
+  LoadQuestionHook(quizInfo.amount, quizInfo.category, setQuestions);
 
   const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {
-    const answer = e.currentTarget.value
-    const correct =  questions[number].correct_answer === answer 
+    const answer = e.currentTarget.value;
+    const correct = questions[number].correct_answer === answer;
     const answerObject = {
-    question: questions[number].question,
-    answer: answer,
-    correct: correct,
-    correctAnswer: questions[number].correct_answer
-   }
-   setUserAnswers(prev => [...prev, answerObject])
-  }
-  
+      question: questions[number].question,
+      answer: answer,
+      correct: correct,
+      correctAnswer: questions[number].correct_answer,
+    };
+    setUserAnswers((prev) => [...prev, answerObject]);
+  };
+
   const nextQuestion = () => {
-    const nextQuestion = number+1
-    setNumber(nextQuestion)
-  }
+    const nextQuestion = number + 1;
+    setNumber(nextQuestion);
+  };
 
   const previousQuestion = () => {
-    const prevQuestion = number-1
-    setNumber(prevQuestion)
-  }
+    const prevQuestion = number - 1;
+    setNumber(prevQuestion);
+  };
 
-    return ( 
-        <div className="quiz text-center bg-dark bg-gradient rounded rounded-4 my-3 mx-4">
-            <h2 className="text-light">Quiz</h2>
-            {questions.length === 0 &&
-            <div>
-              <div className="spinner-border text-danger text-center mx-auto"></div>
-              <div> <h5 className="text-danger">Please wait to load questions...</h5></div>
-            </div>
-            }
-            {questions.length !==0 &&
-            <div className="py-3">
-              <QuestionCard 
-               question={questions[number].question}
-               answers={questions[number].answers}
-               questionNumber={number+1}
-               totalQuestion={quizInfo.amount}
-               userAnswer={userAnswers? userAnswers[number] : undefined}
-               callback={checkAnswer}
-               onNext={nextQuestion}
-               onPrevious={previousQuestion}
-              /> 
-            </div>
-            } 
+  return (
+    <div className="quiz text-center bg-dark bg-gradient rounded rounded-4 my-3 mx-4">
+      <h2 className="text-light">Quiz</h2>
+      {questions.length === 0 && (
+        <div>
+          <div className="spinner-border text-danger text-center mx-auto"></div>
+          <div>
+            {" "}
+            <h5 className="text-danger">Please wait to load questions...</h5>
+          </div>
         </div>
-     )
-}
-  
+      )}
+      {questions.length !== 0 && (
+        <div className="py-3">
+          <QuestionCard
+            question={questions[number].question}
+            answers={questions[number].answers}
+            questionNumber={number + 1}
+            totalQuestion={quizInfo.amount}
+            userAnswer={userAnswers ? userAnswers[number] : undefined}
+            callback={checkAnswer}
+            onNext={nextQuestion}
+            onPrevious={previousQuestion}
+          />
+        </div>
+      )}
+    </div>
+  );
+};
+
 export default Quiz;
