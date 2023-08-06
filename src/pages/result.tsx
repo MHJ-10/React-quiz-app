ea899607cff37a495ecc1c0cf7539290c8a6e046
Mohammad Hossein Jafari <mhj10mhj10jafari@gmail.com>
2023-08-06 16:45:50 +0330

    add quizInfo to result

--- src/pages/result.tsx
+++ src/pages/result.tsx
@@ -1,86 +1,89 @@
 import { useContext } from "react";
 import { Link } from "react-router-dom";
 import { AppContext } from "../hooks/stateContext";
+import quizContext from "../contexts/quizContext";
 
 const Result = () => {
-   const{userAnswers, setUserAnswers, quizInfo, questions, setQuestions } = useContext(AppContext)
-   
-   const renderPercent  = () => {
-    const total = quizInfo.amount
-    let correctLength = 0
-    let inCorrectLength = 0
+  const { userAnswers, setUserAnswers, questions, setQuestions } =
+    useContext(AppContext);
+  const { quizInfo } = useContext(quizContext);
+  const renderPercent = () => {
+    const total = quizInfo.amount;
+    let correctLength = 0;
+    let inCorrectLength = 0;
 
-    for (let i=0 ; i< userAnswers.length ; i++){
-      if(userAnswers[i].correct){
-       correctLength ++
+    for (let i = 0; i < userAnswers.length; i++) {
+      if (userAnswers[i].correct) {
+        correctLength++;
       }
-     }
+    }
 
-    for (let i=0 ; i< userAnswers.length ; i++){
-      if(!userAnswers[i].correct){
-       inCorrectLength ++
+    for (let i = 0; i < userAnswers.length; i++) {
+      if (!userAnswers[i].correct) {
+        inCorrectLength++;
       }
-     }
+    }
 
-    const percent = (((correctLength)- (inCorrectLength/3))/total * 100)
+    const percent = ((correctLength - inCorrectLength / 3) / total) * 100;
 
-    return (
-      <p className="badge bg-primary">{percent.toFixed(2)} %</p>
-    )
-   
-   }
-   
-   const handleReset = () => {
-    setQuestions([])
-    setUserAnswers([])
-   }
+    return <p className="badge bg-primary">{percent.toFixed(2)} %</p>;
+  };
 
-    return(
-        <div className="result text-center mx-auto px-2 my-3">
-          <div className="bg-light bg-gradient bg-opacity-75 d-flex justify-content-around pt-3 border border-light mx-auto text-dark">
-           <h4>Name: {quizInfo.name}</h4>
-           <h4>Overall Percentage: {renderPercent()}</h4>
-          </div>
+  const handleReset = () => {
+    setQuestions([]);
+    setUserAnswers([]);
+  };
 
-          <table className="table mx-auto table-dark table-striped table-hover table-bordered border border-light">
-            <thead>
+  return (
+    <div className="result text-center mx-auto px-2 my-3">
+      <div className="bg-light bg-gradient bg-opacity-75 d-flex justify-content-around pt-3 border border-light mx-auto text-dark">
+        <h4>Name: {quizInfo.name}</h4>
+        <h4>Overall Percentage: {renderPercent()}</h4>
+      </div>
+
+      <table className="table mx-auto table-dark table-striped table-hover table-bordered border border-light">
+        <thead>
+          <tr>
+            <th>Question</th>
+            <th>Your Answer</th>
+            <th>Correct Answer</th>
+            <th>Correct</th>
+          </tr>
+        </thead>
+        <tbody>
+          {questions.map((question) => {
+            const answers = userAnswers.find(
+              (answer) => answer.question === question.question
+            );
+            const correct = answers?.answer === question.correct_answer;
+            const hasAnswer = answers?.answer;
+            return (
               <tr>
-              <th>Question</th>
-              <th>Your Answer</th>
-              <th>Correct Answer</th>
-              <th>Correct</th>
-            </tr>
-            </thead>
-            <tbody>
-              {
-                questions.map((question) => {
-                  const answers = userAnswers.find((answer) => answer.question === question.question)
-                  const correct = answers?.answer === question.correct_answer
-                  const hasAnswer = answers?.answer
-                  return (
-                    <tr>
-                      <td>{question.question}</td>
-                      <td>{answers ? answers.answer : "-"}</td>
-                      <td>{question.correct_answer}</td>
-                      <td className={hasAnswer ?(correct ? "text-success" : "text-danger") : "text-light" }>
-                       {hasAnswer ? correct.toString() : "-"} 
-                      </td>
-                    </tr>
-                  )
-                })
-              }
-            </tbody>
-          </table>
+                <td>{question.question}</td>
+                <td>{answers ? answers.answer : "-"}</td>
+                <td>{question.correct_answer}</td>
+                <td
+                  className={
+                    hasAnswer
+                      ? correct
+                        ? "text-success"
+                        : "text-danger"
+                      : "text-light"
+                  }
+                >
+                  {hasAnswer ? correct.toString() : "-"}
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
 
-          <Link 
-           to={"/"}
-           onClick={handleReset}
-           className="btn btn-lg btn-primary">
-           New Quiz
-          </Link>
-
-        </div>
-    )
-}
+      <Link to={"/"} onClick={handleReset} className="btn btn-lg btn-primary">
+        New Quiz
+      </Link>
+    </div>
+  );
+};
 
 export default Result;
