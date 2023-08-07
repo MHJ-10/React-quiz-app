7b17bc5884fa1d9dfd483dbed5c1e2f209aea104
Mohammad Hossein Jafari <mhj10mhj10jafari@gmail.com>
2023-08-07 17:42:12 +0330

    delete userAnswers and quizInfo

--- src/hooks/stateContext.tsx
+++ src/hooks/stateContext.tsx
@@ -1,56 +1,26 @@
-import React ,{ createContext, useState } from "react";
+import React, { createContext, useState } from "react";
 import { QuestionState } from "../services/api";
 
-export interface AnswerObject {
-    question: string
-    answer: string
-    correct?: boolean 
-    correctAnswer: string
-}
-
-interface QuizInfo {
-    name: string
-    amount: number
-    category: number
-}
-
 interface IAppContext {
-    quizInfo: QuizInfo
-    setQuizInfo: React.Dispatch<React.SetStateAction<QuizInfo>> 
-    userAnswers: AnswerObject[]
-    setUserAnswers: React.Dispatch<React.SetStateAction<AnswerObject[]>>
-    questions: QuestionState[]
-    setQuestions: React.Dispatch<React.SetStateAction<QuestionState[]>>
+  questions: QuestionState[];
+  setQuestions: React.Dispatch<React.SetStateAction<QuestionState[]>>;
 }
 
 interface IAppContextProps {
-    children: React.ReactNode
+  children: React.ReactNode;
 }
 
 export const AppContext = createContext<IAppContext>({
-    quizInfo: {name:"", amount: 5, category: 9},
-    setQuizInfo: () => {},
-    userAnswers: [],
-    setUserAnswers: () => {},
-    questions: [],
-    setQuestions: () => {}
-})
+  questions: [],
+  setQuestions: () => {},
+});
 
-export const StateContext: React.FC<IAppContextProps> = ({children}) => {
-    const [userAnswers, setUserAnswers] = useState<AnswerObject[]>([])
-    const [questions, setQuestions] = useState<QuestionState[]>([])
-    const [quizInfo, setQuizInfo] = useState<QuizInfo>({
-        name: "",
-        amount: 5,
-        category: 9
-    })
+export const StateContext: React.FC<IAppContextProps> = ({ children }) => {
+  const [questions, setQuestions] = useState<QuestionState[]>([]);
 
-    return (
-        <AppContext.Provider value={{quizInfo, setQuizInfo, userAnswers, setUserAnswers, questions, setQuestions}}>
-            {children}
-        </AppContext.Provider>
-   );
-}
-    
-
-
+  return (
+    <AppContext.Provider value={{ questions, setQuestions }}>
+      {children}
+    </AppContext.Provider>
+  );
+};
