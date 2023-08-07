2c3b709782a75c214a80819c401c5d3b76991ea4
Mohammad Hossein Jafari <mhj10mhj10jafari@gmail.com>
2023-08-07 18:05:23 +0330

    add questionsDispatch and remove setQuestions

--- src/hooks/loadQuestionsHook.tsx
+++ src/hooks/loadQuestionsHook.tsx
@@ -1,15 +1,16 @@
-import { useEffect } from "react";
+import { useContext, useEffect } from "react";
 import QuestionService from "../services/api";
+import QuestionsContext from "../contexts/questionsContext";
 
+export const LoadQuestionHook = (amount: number, category: number) => {
+  const { questionsDispatch } = useContext(QuestionsContext);
 
-export const LoadQuestionHook = (amount: number, category: number, setQuestions: any) => {
-  useEffect ( () => {
-    loadQuestions()
-    },[])
-    async function loadQuestions () {
-    const questions = await QuestionService.getQuestions(amount, category)
-    setQuestions(questions)
+  useEffect(() => {
+    loadQuestions();
+  }, []);
+
+  async function loadQuestions() {
+    const questions = await QuestionService.getQuestions(amount, category);
+    questionsDispatch({ type: "SET_QUESTIONS", questions });
   }
-}
- 
-
+};
